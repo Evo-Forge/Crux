084f0a4862e562cdb65ede7ba810905fe895e580
snupa <bunta.adrian@gmail.com>
2015-02-19 08:35:57 +0200

    - added static component

---
+++ boilerplate/static/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  project: {
+    static: {
+      ip: '0.0.0.0',
+      port: 3200,
+      path: 'public/'
+    }
+  },
+  environment: {
+    static: {
+      debug: true
+    }
+  }
+};
